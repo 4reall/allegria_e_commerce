d3630a24192ee7590e863e1c94b92ff0e58db5f6
Ivan <pl_st@list.ru>
2022-11-04 23:50:15 +0300

    Заменил все теги текста на typography

--- client/src/pages/brands.tsx
+++ client/src/pages/brands.tsx
@@ -1,3 +1,4 @@
+import Typography from "common/components/_base/Typography/Typography";
 import { GetStaticPropsContext } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -6,18 +7,18 @@ const Brands = () => {
 	const { t } = useTranslation('brands')
 	return (
 		<section className="vintageBrand relative uppercase px-1 pb-28 mds:px-[15px]">
-			<div className="vintageIntroduction text-center text-beige pt-56 relative z-10 space-y-1 font-decor h-[600px] mb-[120px] mds:h-[460px] mds:pt-40 mds:mb-[50px]">
+			<div className="vintageIntroduction text-center pt-56 relative z-10 space-y-1 h-[600px] mb-[120px] mds:h-[460px] mds:pt-40 mds:mb-[50px]">
 				<div className="vintageIntroductionWrapper">
-					<p className="vintageAnnotation text-[25px] xs:text-base">
+					<Typography type="decor" variant="md" color="beige" className="vintageAnnotation block xs:text-base">
 						{t('brand')}
-					</p>
-					<h1 className="vintageTitle text-xl mds:text-lg">
+					</Typography>
+					<Typography type="decor" variant="xl" color="beige" className="vintageTitle mds:text-lg">
 						{t('vintageBrand')}
-					</h1>
+					</Typography>
 				</div>
-				<p className="vintageSubtitle m-auto font-avenir normal-case md:w-[637px] xs:w-50%">
+				<Typography type="avenir" variant="md" color="beige" className="vintageSubtitle m-auto normal-case md:w-[637px] xs:w-50%">
 					{t('vintageDescriprion')}
-				</p>
+				</Typography>
 			</div>
 			<div>
 				<img className="vintageBGImage absolute top-0 left-0 object-cover w-full h-[460px] object-top md:h-[600px]" src="/assets/brands/vintageBrand/americanVintageBanner.jpg" alt="vintageBanner" />
@@ -26,12 +27,12 @@ const Brands = () => {
 				<img className="mr-[6%] my-auto mds:w-[260px] lgs:w-[300px] mds:max-h-[230px] mds:mx-auto mds:max-w-[255px]" src="/assets/brands/vintageBrand/americanVintageText.png" alt="woman in white coat" />
 				<img className="mx-auto mb-[20%] hidden mds:block" src="/assets/brands/vintageBrand/americanVintageForSmallScreen.png" alt="vintage Woman Sitting for small screens" />
 				<div className="vintageTextWrapper my-auto font-avenir text-primary-dark mds:text-center mds:mb-[10%]">
-					<h2 className="vintageTextTitle font-extrabold text-[25px] pb-8 mds:text-[20px]">
+					<Typography type="avenir" variant="lg" color="primaryDark" className="vintageTextTitle block font-extrabold pb-8 mds:text-[20px]">
 						{t('vintageBrand')}
-						</h2>
-					<p className="vintageTextDescription text-[14px] max-w-[627px] mds:mx-auto">
+						</Typography>
+					<Typography variant="sm" type="avenir" className="vintageTextDescription max-w-[627px] mds:mx-auto">
 						{t('vintageText')}
-						</p>
+						</Typography>
 				</div>
 			</div>
 			<img className="ml-[55%] mt-10 mds:hidden" src="/assets/brands/vintageBrand/americanVintageFooterPic.png" alt="vintage Woman Sitting Footer" />
