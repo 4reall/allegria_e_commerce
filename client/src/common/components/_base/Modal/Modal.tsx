47f63062d115fbe75aa343aa90bf2f47c21a8b78
4reall <1l1b1v1f1@bk.ru>
2022-11-07 12:12:16 +0300

    hotfix

--- client/src/common/components/_base/Modal/Modal.tsx
+++ client/src/common/components/_base/Modal/Modal.tsx
@@ -1,88 +1,85 @@
-import { Transition } from "react-transition-group";
-import * as DialogPrimitive from "@radix-ui/react-dialog";
-import cn from "classnames";
-import React, {
-  Fragment,
-  PropsWithChildren,
-  useRef,
-  useState,
-} from "react";
-import CloseButton from "../CloseButton";
+import { Transition } from 'react-transition-group';
+import * as DialogPrimitive from '@radix-ui/react-dialog';
+import cn from 'classnames';
+import React, { Fragment, PropsWithChildren, useRef, useState } from 'react';
+import CloseButton from '../CloseButton';
 
 interface ModalProps {
-  trigger: React.ReactNode;
-  closeHandler?: () => void;
-  isOpen?: boolean;
-  setIsOpen?: () => void;
+	trigger: React.ReactNode;
+	closeHandler?: () => void;
+	isOpen?: boolean;
+	setIsOpen?: () => void;
 }
 
 const duration = 300;
 const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0,
+	transition: `opacity ${duration}ms ease-in-out`,
+	opacity: 0,
 };
 
 const transitionStyles = {
-  entering: { opacity: 1 },
-  entered: { opacity: 1 },
-  exiting: { opacity: 0 },
-  exited: { opacity: 0 },
+	entering: { opacity: 1 },
+	entered: { opacity: 1 },
+	exiting: { opacity: 0 },
+	exited: { opacity: 0 },
 };
 
 const Modal = ({
-  trigger,
-  isOpen = undefined,
-  setIsOpen = undefined,
-  children,
+	trigger,
+	isOpen,
+	setIsOpen,
+	children,
 }: PropsWithChildren<ModalProps>) => {
-  const [isOpen_, setIsOpen_] = useState(false);
-  const nodeRef = useRef(null);
+	const [isOpen_, setIsOpen_] = useState(false);
+	const nodeRef = useRef(null);
 
-  return (
-    <Transition
-      nodeRef={nodeRef}
-      in={isOpen ? isOpen : isOpen_}
-      timeout={duration}
-    >
-      {(state) => (
-        <DialogPrimitive.Root
-          open={isOpen ? isOpen : isOpen_}
-          onOpenChange={setIsOpen ? setIsOpen : setIsOpen_}
-        >
-          <DialogPrimitive.Trigger asChild>
-            {trigger}
-          </DialogPrimitive.Trigger>
+	return (
+		<Transition
+			nodeRef={nodeRef}
+			in={isOpen === undefined ? isOpen : isOpen_}
+			timeout={duration}
+		>
+			{(state) => (
+				<DialogPrimitive.Root
+					open={isOpen === undefined ? isOpen : isOpen_}
+					onOpenChange={
+						setIsOpen === undefined ? setIsOpen : setIsOpen_
+					}
+				>
+					<DialogPrimitive.Trigger asChild>
+						{trigger}
+					</DialogPrimitive.Trigger>
 
-          <DialogPrimitive.Portal>
-            <DialogPrimitive.Overlay
-              forceMount
-              className="fixed inset-0 z-20 bg-black/20"
-            />
-            <DialogPrimitive.Content
-              ref={nodeRef}
-              className={cn(
-                "fixed z-50",
-                "h-[100vh] w-[100vw] max-w-lg py-7 px-10 md:h-auto md:w-auto",
-                "top-[50%] left-[50%]  -translate-x-[50%] -translate-y-[50%] ",
-                " bg-white",
-                "border-none",
-                { ...defaultStyle },
-                { ...transitionStyles }
-              )}
-            >
-              <DialogPrimitive.Description>
-                {children}
-              </DialogPrimitive.Description>
+					<DialogPrimitive.Portal>
+						<DialogPrimitive.Overlay
+							forceMount
+							className="fixed inset-0 z-20 bg-black/20"
+						/>
+						<DialogPrimitive.Content
+							ref={nodeRef}
+							className={cn(
+								'fixed z-50',
+								'h-[100vh] w-[100vw] max-w-lg py-7 px-10 md:h-auto md:w-auto',
+								'top-[50%] left-[50%]  -translate-x-[50%] -translate-y-[50%] ',
+								' bg-white',
+								'border-none',
+								{ ...defaultStyle },
+								{ ...transitionStyles }
+							)}
+						>
+							<DialogPrimitive.Description>
+								{children}
+							</DialogPrimitive.Description>
 
-              <DialogPrimitive.Close>
-                <CloseButton className="absolute right-7 top-7" />
-              </DialogPrimitive.Close>
-            </DialogPrimitive.Content>
-          </DialogPrimitive.Portal>
-        </DialogPrimitive.Root>
-      )}
-    </Transition>
-  );
+							<DialogPrimitive.Close>
+								<CloseButton className="absolute right-7 top-7" />
+							</DialogPrimitive.Close>
+						</DialogPrimitive.Content>
+					</DialogPrimitive.Portal>
+				</DialogPrimitive.Root>
+			)}
+		</Transition>
+	);
 };
 
 export default Modal;
--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,169 +1,165 @@
-import Typography from "common/components/_base/Typography/Typography";
-import React from "react";
-import CustomAccordion from "common/components/CustomAccordion/CustomAccordion";
-import RoundLabel from "common/components/_base/RoundLabel";
-import PageContainer from "common/components/_base/PageContainer";
-import ProductCard from "common/components/ProductCard/ProductCard";
-import { HeartIcon } from "@radix-ui/react-icons";
-import AccordionItem from "common/components/CustomAccordion/AccordionItem";
+import Typography from 'common/components/_base/Typography/Typography';
+import React from 'react';
+import CustomAccordion from 'common/components/CustomAccordion/CustomAccordion';
+import RoundLabel from 'common/components/_base/RoundLabel';
+import PageContainer from 'common/components/_base/PageContainer';
+import ProductCard from 'common/components/ProductCard/ProductCard';
+import { HeartIcon } from '@radix-ui/react-icons';
+import AccordionItem from 'common/components/CustomAccordion/AccordionItem';
 
 export const DummyHeader = () => {
-  return (
-    <header className="mb-[8px] h-[60px] bg-accent">
-      <PageContainer>
-        <div className="flex justify-between">
-          <span>LOGO</span>
-          <span>ICONS</span>
-        </div>
-      </PageContainer>
-    </header>
-  );
+	return (
+		<header className="bg-accent mb-[8px] h-[60px]">
+			<PageContainer>
+				<div className="flex justify-between">
+					<span>LOGO</span>
+					<span>ICONS</span>
+				</div>
+			</PageContainer>
+		</header>
+	);
 };
 
 const DummyBread = () => {
-  return <div className="mb-[23px] h-[14px] bg-accent"></div>;
+	return <div className="bg-accent mb-[23px] h-[14px]"></div>;
 };
 
 const DummyFilterSection = () => {
-  return <div className="mb-[24px] h-[40px] bg-accent"></div>;
+	return <div className="bg-accent mb-[24px] h-[40px]"></div>;
 };
 
 const DummyInfoSection = () => {
-  return <div className="mb-[18px] h-[24px] bg-accent"></div>;
+	return <div className="bg-accent mb-[18px] h-[24px]"></div>;
 };
 
 const PRODUCTS = [
-  {
-    image:
-      "https://img.ltwebstatic.com/images3_pi/2021/08/13/1628835748ca6689f27e457e5c43679227513cd395.webp",
-    brand: "george gina lucy",
-    name: " Classic shoes",
-    price: 1200,
-  },
-  {
-    image:
-      "https://img.ltwebstatic.com/images3_pi/2021/10/10/16338051716d38e86edbc84ad5f25d875e1dd0c658.webp",
-    brand: "george gina lucy",
-    name: " Classic shoes",
-    price: 1200,
-  },
-  {
-    image:
-      "https://img.ltwebstatic.com/images3_pi/2021/08/13/1628835748ca6689f27e457e5c43679227513cd395.webp",
-    brand: "george gina lucy",
-    name: " Classic shoes",
-    price: 1200,
-  },
-  {
-    image:
-      "https://img.ltwebstatic.com/images3_pi/2021/08/13/1628835748ca6689f27e457e5c43679227513cd395.webp",
-    brand: "george gina lucy",
-    name: " Classic shoes",
-    price: 1200,
-  },
+	{
+		image: 'https://img.ltwebstatic.com/images3_pi/2021/08/13/1628835748ca6689f27e457e5c43679227513cd395.webp',
+		brand: 'george gina lucy',
+		name: ' Classic shoes',
+		price: 1200,
+	},
+	{
+		image: 'https://img.ltwebstatic.com/images3_pi/2021/10/10/16338051716d38e86edbc84ad5f25d875e1dd0c658.webp',
+		brand: 'george gina lucy',
+		name: ' Classic shoes',
+		price: 1200,
+	},
+	{
+		image: 'https://img.ltwebstatic.com/images3_pi/2021/08/13/1628835748ca6689f27e457e5c43679227513cd395.webp',
+		brand: 'george gina lucy',
+		name: ' Classic shoes',
+		price: 1200,
+	},
+	{
+		image: 'https://img.ltwebstatic.com/images3_pi/2021/08/13/1628835748ca6689f27e457e5c43679227513cd395.webp',
+		brand: 'george gina lucy',
+		name: ' Classic shoes',
+		price: 1200,
+	},
 ];
 
 const ProductsPage = () => {
-  const categories = [
-    {
-      title: "ОДЕЖДА",
-      content: [
-        "Свитера толcтовки",
-        "Платья юбки",
-        "Футболки и топы",
-        "Брюки и шорты",
-        "Рубашки",
-        "Комбинезоны",
-        "Леггинсы",
-      ],
-    },
-    {
-      title: "Обувь",
-      content: ["Кроссовки", "Шлепанцы"],
-    },
-    {
-      title: "СУМКИ",
-      content: [
-        "Сумки",
-        "Рюкзаки",
-        "Кроссбоди",
-        "Поясные",
-        "Спортивные",
-        "Шопперы",
-      ],
-    },
-    {
-      title: "АКСЕСУАРЫ",
-      content: [
-        "Головные уборы",
-        "Перчатки",
-        "Шарфы и платки",
-        "Носки",
-        "Гетры",
-      ],
-    },
-    {
-      title: "БЕЛЬЕ",
-      content: ["Разное"],
-    },
-  ];
+	const categories = [
+		{
+			title: 'ОДЕЖДА',
+			content: [
+				'Свитера толcтовки',
+				'Платья юбки',
+				'Футболки и топы',
+				'Брюки и шорты',
+				'Рубашки',
+				'Комбинезоны',
+				'Леггинсы',
+			],
+		},
+		{
+			title: 'Обувь',
+			content: ['Кроссовки', 'Шлепанцы'],
+		},
+		{
+			title: 'СУМКИ',
+			content: [
+				'Сумки',
+				'Рюкзаки',
+				'Кроссбоди',
+				'Поясные',
+				'Спортивные',
+				'Шопперы',
+			],
+		},
+		{
+			title: 'АКСЕСУАРЫ',
+			content: [
+				'Головные уборы',
+				'Перчатки',
+				'Шарфы и платки',
+				'Носки',
+				'Гетры',
+			],
+		},
+		{
+			title: 'БЕЛЬЕ',
+			content: ['Разное'],
+		},
+	];
 
-  return (
-    <>
-      <DummyHeader />
-      <DummyBread />
-      <PageContainer>
-        <div className="flex justify-center gap-6">
-          <aside className="hidden  w-1/6 lg:block ">
-            <CustomAccordion
-              content={categories}
-              renderItem={(item, index) => (
-                <AccordionItem
-                  item={item}
-                  index={index}
-                  isUppercase={true}
-                />
-              )}
-            />
-          </aside>
-          <section className="w-full max-w-4xl">
-            <Typography
-              as="h2"
-              uppercase
-              className="mb-11"
-              variant="2xl"
-            >
-              Кофты и пиджаки
-            </Typography>
-            <DummyFilterSection />
-            <DummyInfoSection />
+	return (
+		<>
+			<DummyHeader />
+			<DummyBread />
+			<PageContainer>
+				<div className="flex justify-center gap-6">
+					<aside className="hidden  w-1/6 lg:block ">
+						<CustomAccordion
+							content={categories}
+							renderItem={(item, index) => (
+								<AccordionItem
+									item={item}
+									index={index}
+									isUppercase={true}
+								/>
+							)}
+						/>
+					</aside>
+					<section className="w-full max-w-4xl">
+						<Typography
+							as="h2"
+							uppercase
+							className="mb-11"
+							variant="2xl"
+						>
+							Кофты и пиджаки
+						</Typography>
+						<DummyFilterSection />
+						<DummyInfoSection />
 
-            <div className="grid grid-cols-2 gap-x-3.5 gap-y-10 md:grid-cols-3 lg:grid-cols-3">
-              {PRODUCTS.map((item, index) => (
-                <ProductCard
-                  key={index}
-                  {...item}
-                  topLabel={
-                    <RoundLabel className="absolute top-2.5 right-2.5 z-10 text-white">
-                      40%
-                    </RoundLabel>
-                  }
-                  bottomLabel={
-                    <RoundLabel
-                      color="white"
-                      className="absolute bottom-2.5 right-2.5 z-10"
-                    >
-                      <HeartIcon width={20} height={20} />
-                    </RoundLabel>
-                  }
-                />
-              ))}
-            </div>
-          </section>
-        </div>
-      </PageContainer>
-    </>
-  );
+						<div className="xs:grid-cols-2 grid grid-cols-1 gap-x-3.5 gap-y-10 md:grid-cols-3 lg:grid-cols-3">
+							{PRODUCTS.map((item, index) => (
+								<ProductCard
+									key={index}
+									{...item}
+									topLabel={
+										<RoundLabel className="absolute top-2.5 right-2.5 z-10 text-white">
+											40%
+										</RoundLabel>
+									}
+									bottomLabel={
+										<RoundLabel
+											color="white"
+											className="absolute bottom-2.5 right-2.5 z-10"
+										>
+											<HeartIcon width={20} height={20} />
+										</RoundLabel>
+									}
+								/>
+							))}
+						</div>
+					</section>
+				</div>
+			</PageContainer>
+		</>
+	);
 };
 
 export default ProductsPage;
